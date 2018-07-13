985b983e0c08d5fa59d818db7c3940870f56b748
unknown <goodbeysad@sina.cn>
2018-07-13 17:57:17 +0800

    new first

---
+++ 测试/boss/js/base.js
@@ -0,0 +1,6 @@
+!function(){
+	document.documentElement.style.fontSize=document.documentElement.clientWidth/7.2+"px";
+}(window)
+window.onresize=function(){
+	document.documentElement.style.fontSize=document.documentElement.clientWidth/7.2+"px";
+}
